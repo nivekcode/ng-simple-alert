9556d8dfa57c471e1e8ee0723deecd018697cde8
u806762 <kevin.kreuzer@mobi.ch>
2019-05-08 08:43:17 +0200

    test(alertservice): add tests for the alert service

--- projects/ng-alert/src/lib/ng-alert.service.spec.ts
+++ projects/ng-alert/src/lib/ng-alert.service.spec.ts
@@ -1,12 +1,48 @@
-import { TestBed } from '@angular/core/testing';
-
-import { NgAlertService } from './ng-alert.service';
+import {NgAlertService} from './ng-alert.service';
+import {TestScheduler} from 'rxjs/testing';
 
 describe('NgAlertService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: NgAlertService = TestBed.get(NgAlertService);
-    expect(service).toBeTruthy();
+  let sut: NgAlertService;
+  let scheduler: TestScheduler;
+
+  beforeEach(() => {
+    sut = new NgAlertService();
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
   });
+
+  it('should stream a success message', () => {
+    const message = 'Vinicius Junior - next worlds best footballer';
+    const expectedMessage = {prefix: 'Success', message, color: 'green'};
+
+    sut.createSuccessAlert(message);
+    scheduler.expectObservable(sut.alertMessage$).toBe('a', {a: expectedMessage});
+  });
+
+  it('should stream a danger message', () => {
+    const message = 'Vinicius Junior - next worlds best footballer';
+    const expectedMessage = {prefix: 'Danger', message, color: 'red'};
+
+    sut.createSuccessAlert(message);
+    scheduler.expectObservable(sut.alertMessage$).toBe('a', {a: expectedMessage});
+  });
+
+  it('should stream a warning message', () => {
+    const message = 'Vinicius Junior - next worlds best footballer';
+    const expectedMessage = {prefix: 'Warning', message, color: 'darkorange'};
+
+    sut.createSuccessAlert(message);
+    scheduler.expectObservable(sut.alertMessage$).toBe('a', {a: expectedMessage});
+  });
+
+  it('should stream a info message', () => {
+    const message = 'Vinicius Junior - next worlds best footballer';
+    const expectedMessage = {prefix: 'Info', message, color: 'dodgerblue'};
+
+    sut.createSuccessAlert(message);
+    scheduler.expectObservable(sut.alertMessage$).toBe('a', {a: expectedMessage});
+  });
+
 });
